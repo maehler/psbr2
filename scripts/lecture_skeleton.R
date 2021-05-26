767da241c3f5382ab4377357c1d16f94cba71291
Niklas Mähler <niklas.mahler@gmail.com>
2021-05-26 12:06:32 +0200

    Add script for creating presentation skeletons

---
+++ scripts/lecture_skeleton.R
@@ -0,0 +1,62 @@
+suppressPackageStartupMessages({
+  library(conflicted)
+  library(dplyr)
+  library(fs)
+  library(here)
+  library(purrr)
+  library(readr)
+  library(stringr)
+  library(yaml)
+})
+
+source(here("scripts/date_functions.R"))
+
+"usage: lecture_skeleton.R [-h]
+
+Create skeletons for all lectures based on -schedule.yml.
+All relative paths will be resolved in relation to the
+RStudio project root directory.
+
+OPTIONS:
+
+  -t --template FILE  template to use [default: lectures/template.Rmd]
+  -o --outdir DIR     output directory [default: lectures]
+  -f --force          create a skeleton even if a file exists
+  -h                  show this help text and exit" -> doc
+
+args <- docopt::docopt(doc)
+
+if (!file_exists(here("_schedule.yml"))) {
+  stop("_schedule.yml not found")
+}
+
+schedule_data <- read_yaml(here("_schedule.yml"))
+topic_df <- tibble(
+  topic = flatten_chr(map(schedule_data$schedule,
+                          ~ map_chr(.$day, ~ .$topic))),
+  course_day = flatten_int(map2(
+    schedule_data$schedule, seq_along(schedule_data$schedule),
+    ~ rep(.y, length(.x$day)))),
+  date = add_business_day(schedule_data$start_date, course_day-1),
+  topic_slug = str_to_lower(topic) %>%
+    str_replace_all("[-: ]", "_") %>%
+    str_replace_all("['\"]", "") %>%
+    str_replace_all("_+", "_"),
+  topic_filename = path(
+    args$outdir,
+    str_c(sprintf("%02d", seq_along(topic)),
+          "_", topic_slug, ".Rmd")) %>%
+    here()
+)
+
+pwalk(topic_df, ~ {
+  if (file_exists(..5)) {
+    return
+  }
+  template_lines <- read_lines(here(args$template))
+  script_lines <- template_lines %>%
+    str_replace("\\{\\{topic\\}\\}", ..1) %>%
+    str_replace("\\{\\{date\\}\\}", format(..3, format = "%Y-%m-%d")) %>%
+    str_replace("\\{\\{topic_slug\\}\\}", ..4)
+  write_lines(script_lines, file = ..5)
+})
