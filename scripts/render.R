a42b23789b68d4378728d046b7b0e68b1ffb2d01
Niklas Mähler <niklas.mahler@gmail.com>
2021-05-26 16:26:52 +0200

    Render both site and lectures

--- scripts/render.R
+++ scripts/render.R
@@ -8,33 +8,41 @@ suppressPackageStartupMessages({
 
 `%>%` <- dplyr::`%>%`
 
-"usage: render.R [-h] DAY [DAY ...]
+"Render site
 
-OPTIONS:
+Usage:
+  render.R lecture N [N ...]
+  render.R site
+  render.R (-h | --help)
 
+Options:
   -d --dir DIR     directory containing Rmd files to render [default: lectures]
-  -o --outdir DIR  output directory for rendered files [default: docs/lectures]
+  -o --outdir DIR  output directory for rendered files [default: lectures]
   -h               show this help text and exit" -> doc
 
 args <- docopt::docopt(doc)
 
-if (length(args$DAY) == 1 && args$DAY == "all") {
-  fs::dir_ls(here(args$dir), type = "file", glob = "*.Rmd") %>% 
-    Filter(x = ., f = function(x) !str_detect(basename(x), "^_")) %>% 
-    walk(~ {
-      render(., output_dir = args$outdir)
+if (args$site) {
+  render_site()
+} else if (args$lecture) {
+  if (length(args$N) == 1 && args$N == "all") {
+    fs::dir_ls(here(args$dir), type = "file", glob = "*.Rmd") %>% 
+      Filter(x = ., f = function(x) !str_detect(basename(x), "^_")) %>% 
+      walk(~ {
+        render(., output_dir = args$outdir)
+      })
+  } else {
+    walk(args$N, ~ {
+      rmd_filename <- fs::dir_ls(
+        here(args$dir), type = "file",
+        glob = str_c(sprintf("*/%02d", as.integer(.)), "*.Rmd"))
+      cat(rmd_filename, '\n')
+      if (length(rmd_filename) == 0) {
+        stop(str_c("no lecture ", ., " found"))
+      } else if (length(rmd_filename) > 1) {
+        stop(str_c("more than one file found for lecture ", .))
+      }
+      render(names(rmd_filename), output_dir = args$outdir)
     })
-} else {
-  walk(args$DAY, ~ {
-    rmd_filename <- fs::dir_ls(
-      here(args$dir), type = "file",
-      glob = str_c(sprintf("*/%02d", as.integer(.)), "*.Rmd"))
-    cat(rmd_filename, '\n')
-    if (length(rmd_filename) == 0) {
-      stop(str_c("no lecture found for day ", .))
-    } else if (length(rmd_filename) > 1) {
-      stop(str_c("more than one file found for day ", .))
-    }
-    render(names(rmd_filename), output_dir = args$outdir)
-  })
+  }
 }
