623c6944b634d65bf772ec3f8383a7adbae01667
Niklas Mähler <niklas.mahler@gmail.com>
2021-08-31 16:52:24 +0200

    Add first draft for reproducibility lecture
    In doing this, I realised that I started to venture into topics that
    will come later in the course, so therefore I immediately moved these
    parts to those lectures (16, 17, and 18). These, of course, have to be
    fleshed out more.

--- lectures/js/macros.js
+++ lectures/js/macros.js
@@ -1,4 +1,11 @@
-remark.macros.scale = function(w) {
+remark.macros.scale = function(w, alt, caption) {
+  if (typeof(alt) === "undefined") {
+    console.error("alt text required");
+  }
   var url = this;
-  return '<img src="' + url + '" style="width: ' + w + ';" />';
+  var img_tag = `<img src="${url}" alt="${alt}" style="width: ${w};" />`;
+  if (typeof(caption) !== "undefined") {
+    return `<figure>${img_tag}<figcaption>${caption}</figcaption></figure>`;
+  }
+  return img_tag;
 };
